f5203b91de88cc1cfa7c8a534c0791790b9bd31a
sainoxcod <sainox.fun@gmail.com>
2026-02-25 15:06:57 +0100

    changements de cons

---
+++ Astraya2.0/settings.py
@@ -0,0 +1,29 @@
+WIDTH    = 1280	
+HEIGTH   = 720
+SCALE = 2
+FPS      = 60
+TILESIZE = 64
+HITBOX_OFFSET = {
+	'player': -26,
+	'object': -40,
+	'grass': -10,
+	'invisible': 0}
+
+# ui 
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = '../graphics/font/joystix.ttf'
+UI_FONT_SIZE = 18
+
+# general colors
+
+# ui colors
+HEALTH_COLOR = 'red'
+HOTBAR_ACTIVE = 'gold'
+
+#world_gen
+SIZE = 2000
+NB_VILLAGES = 80
+SEED = 0
