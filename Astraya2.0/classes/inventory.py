0cde9775fc02e35be6380e2933e6a82d5142a399
sainoxcod <sainox.fun@gmail.com>
2026-03-14 20:12:32 +0100

    dsl j'ai enlevé ton truc jaguard du 30k

--- Astraya2.0/classes/inventory.py
+++ Astraya2.0/classes/inventory.py
@@ -129,7 +129,7 @@ class Inventory:
         """Drop from current hotbar slot. Returns (item, quantity) or None."""
         return self.drop_slot(self.selected_hotbar, quantity)
 
-    def try_place_selected(self, current_map, tile_x, tile_y, block_grp, player_pos):
+    def try_place_selected(self, current_map, tile_x, tile_y, player_pos):
         """If selected item is a block, place it at (tile_x, tile_y). Returns True if placed."""
         from .items import ItemType
         item = self.selected_item
@@ -149,10 +149,7 @@ class Inventory:
             return False
         # Don't place on ocean (0) or other blocking tiles if we want walkable placement only
         
-        for block in block_grp:
-            if block.tile_x == tile_x and block.tile_y == tile_y:
-                return False
-        block_grp.add(ent.Block(texture.BLOCK_TEXTURE[item.place_biome_id], current_map, x=tile_x, y =tile_y))
+        current_map[tile_y, tile_x] = item.place_biome_id
         self.remove_slot_item(self.selected_hotbar, 1)
         return True
 
